05053c3c028932ab51ac768118a6dd6fdbabc345
KeroBeros68 <kevinbertrand68.pro@gmail.com>
2025-03-12 15:43:30 +0100

    Update

--- Backend/middleware/sharp-config.js
+++ Backend/middleware/sharp-config.js
@@ -10,7 +10,7 @@ const optimizeImg = async (req, res, next) => {
     const newFilePath = `images/${newName}`;
     try {
         await sharp(filePath)
-            .resize(400, 500, { fit: 'contain', background: { r: 255, g: 255, b: 255 } })
+            .resize(400, 500)
             .toFormat('webp')
             .webp({ quality: 80 })
             .toFile(newFilePath);
